df60eaf5e858ef6b4e9e50935d3f9623807a3b60
Worravut <admin@example.com>
2019-09-05 15:20:18 +0700

    upadte

--- src/pages/Number.jsx
+++ src/pages/Number.jsx
@@ -2,13 +2,18 @@ import React, { useState } from 'react'
 import Axios from 'axios'
 
 const url = 'http://localhost:3001'
+const number = 0
 
 export default function Number() {
 
     const [ token,setToken ] = useState('')
-    async function handleLogin() {
+    const [ username,setUsername ] = useState('')
+    const [ data,setData ] = useState('')
+
+    async function handleLogin(e) {
         const res = await Axios.post(url+'/login', { username: 'admin'})
         setToken(res.data.token)
+        // alert(`User `+username)
     }
 
     async function handleClick() {
@@ -18,11 +23,21 @@ export default function Number() {
             }
         })
         console.log(res.data)
+        setData(res.data)
     }
     return <div>
         <button className="button" onClick={handleLogin}>Login</button>
+        {/* <form onSubmit={handleLogin}>
+            <input className="input" name="username" defaultValue={username} type="text"/>
+            <button className="button is-primary" type="submit">Sign Up</button>
+        </form> */}
+        
+        <pre>{ token ? "Pass":"Fail"}</pre>
+        <hr/>
+        <pre>{ token }</pre>
         <hr/>
         <h1 className="title">Number</h1>
         <button className="button" onClick={handleClick}>Get Number</button>
+        <pre>{ data }</pre>
     </div>
 }
