53cd0705ef6ba506cb7e5ebb396e8ce01213c378
Worravut <admin@example.com>
2019-09-05 11:51:14 +0700

    udpate handle Save

--- src/pages/UserProfile.jsx
+++ src/pages/UserProfile.jsx
@@ -9,12 +9,45 @@ export default function UserProfile(props) {
     const [ user, setUser ] = useState({});
 
     async function handleClick() {
-        const data = await Axios.get(url+userId)
-        setUser(data)
+        const res = await Axios.get(url+userId)
+        setUser(res.data)
     }
+    function handleChange(e) {
+        const fieldName = e.target.name
+        const value = e.target.value
+
+        const newUser = {
+            ...user,
+            [fieldName]: value
+        }
+        setUser(newUser)
+    }
+
+    async function handleSave() {
+        // insert
+        //// const res = await Axios.post(url, user)
+        //// console.log(res)
+
+        // update
+        //// const res = await Axios.put(url+userId, user)
+        //// console.log(res)
+        
+        //delete 
+        const res = await Axios.delete(url+userId)
+        console.log(res)
+    }
+
     return <div>
         <h1 className="title">User Profile</h1>
-        <p><button className="button is-primary" onClick={handleClick}>Get user id { userId }</button></p>
-        <pre>{ JSON.stringify(user.data) }</pre>
+        <button className="button is-primary" onClick={handleSave}>Save</button>
+        <button className="button" onClick={handleClick}>Get user id { userId }</button>
+        <hr/>
+        <pre>{ JSON.stringify(user) }</pre>
+        <div className="field">
+            <input defaultValue= { user.name } onChange={ handleChange } name="name" type="text" className="input"/>
+        </div>
+        <div className="field">
+            <input defaultValue= { user.email } onChange={ handleChange } name="email" type="text" className="input"/>
+        </div>
     </div>
 }
